10b9707c1927c10dd5f2c87c0372e68b73edd71b
marcustakeyasu <116593545+marcustakeyasu@users.noreply.github.com>
2024-04-03 22:08:07 -0300

    Delete microservicos/microservicos directory

--- microservicos/microservicos/src/main/java/com/ac1_arquitetura_web/Repository/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ac1_arquitetura_web.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ac1_arquitetura_web.Aluno.Aluno;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long>  {
-    
-}
